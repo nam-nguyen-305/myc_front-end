392ae8109747e91ea29f3377196f2464698334aa
namnp-1182 <nguyen.phuong.nam-c@sun-asterisk.com>
2023-01-30 14:09:36 +0700

    setup eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export const App = ()=>{
+  return <div className="App">
+
+         </div>;
+};
 root.render(
   <React.StrictMode>
     <App />
